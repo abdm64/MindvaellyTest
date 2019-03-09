4c4bf3555c5883cc4be5ed8e07042c9038dc8f8b
Messlleka Abdellah <39844646+abdm64@users.noreply.github.com>
2019-03-09 22:40:36 +0100

    Initial Commit

---
+++ testHttpreq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testHttpreq
+//
+//  Created by ABD on 09/03/2019.
+//  Copyright © 2019 ABD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
