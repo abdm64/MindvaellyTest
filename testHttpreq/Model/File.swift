6079eea9ec91b2cc578bb168977f77a7f8f841ae
Messlleka Abdellah <39844646+abdm64@users.noreply.github.com>
2019-03-15 21:11:11 +0100

    second commit

---
+++ testHttpreq/Model/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  testHttpreq
+//
+//  Created by ABD on 14/03/2019.
+//  Copyright © 2019 ABD. All rights reserved.
+//
+
+import Foundation
---
+++ testHttpreq/View/CellButton.swift
@@ -0,0 +1,21 @@
+//
+//  CellButton.swift
+//  testHttpreq
+//
+//  Created by ABD on 14/03/2019.
+//  Copyright © 2019 ABD. All rights reserved.
+//
+
+import UIKit
+
+class CellButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
